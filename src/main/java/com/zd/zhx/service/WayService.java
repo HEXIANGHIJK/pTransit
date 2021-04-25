8ea3ab94509c01444753b6af33139131b9599d8a
Idea Admin <zhx0204@hotmail.com>
2021-04-25 09:45:38 +0800

    2021.4.25ys

--- src/main/java/com/zd/zhx/service/WayService.java
+++ src/main/java/com/zd/zhx/service/WayService.java
@@ -1,8 +1,10 @@
 package com.zd.zhx.service;
 
+import com.zd.zhx.pojo.Walking;
 import com.zd.zhx.pojo.Way;
 
 public interface WayService {
 
-
+    Double getWalkTime(Double userSpeed, Walking walking);
+    Double getWalkTime(Double userSpeed, Way way,int tranCount,int segCount);
 }
--- src/main/java/com/zd/zhx/service/impl/WayServiceImpl.java
+++ src/main/java/com/zd/zhx/service/impl/WayServiceImpl.java
@@ -1,6 +1,45 @@
 package com.zd.zhx.service.impl;
 
+import com.zd.zhx.pojo.*;
 import com.zd.zhx.service.WayService;
 
+import java.text.DecimalFormat;
+import java.util.List;
+
 public class WayServiceImpl implements WayService {
+    @Override
+    public Double getWalkTime(Double userSpeed, Walking walking) {
+        String destinationStr = walking.getDestination();
+        double destination = 0;
+        if (destinationStr!=null){
+             destination=Double.parseDouble(destinationStr);
+             Double time=destination/userSpeed;
+             DecimalFormat df = new DecimalFormat("#.00");
+
+            return Double.valueOf(df.format(time));
+        }
+        else
+            return null;
+    }
+
+    @Override
+    public Double getWalkTime(Double userSpeed, Way way, int tranCount, int segCount) {
+        Route route = way.getRoute();
+        Transit transits = route.getTransits().get(tranCount);
+        Segment segment = transits.getSegments().get(segCount);
+        Walking walking = segment.getWalking();
+
+        String destinationStr = walking.getDestination();
+        double destination = 0;
+        if (destinationStr!=null){
+            destination=Double.parseDouble(destinationStr);
+            Double time=destination/userSpeed;
+            DecimalFormat df = new DecimalFormat("#.00");
+
+            return Double.valueOf(df.format(time));
+        }
+        else
+            return null;
+    }
+
 }
